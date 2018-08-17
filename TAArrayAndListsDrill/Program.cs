cc445bf664953c1f7e4072f2ec5f7a70c41e1cf6
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-17 13:42:34 -0400

    Add project files.

---
+++ TAArrayAndListsDrill/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAArrayAndListsDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
