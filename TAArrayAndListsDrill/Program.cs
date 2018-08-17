5c14498d25510f684fe6e4261e1ef556ebb2e170
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-17 15:53:53 -0400

    Array and List test

--- TAArrayAndListsDrill/Program.cs
+++ TAArrayAndListsDrill/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TAArrayAndListsDrill
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string[] stringArray = { "Hello", "How", "Are", "You" };
+        Console.WriteLine("Hello, How, Are, You");
+        Console.WriteLine("Select an Index of the Array.");
+        int index = Convert.ToInt32(Console.ReadLine());
+        
+        if (index > 3)
         {
+            Console.WriteLine("That is out of the index range!");
+            Console.ReadLine();
+            return;
         }
+        else
+        {
+            Console.WriteLine(stringArray[index]);           
+        }
+
+        
+        int[] intArray = { 1, 5, 6, 13, 24 };
+        Console.WriteLine("1, 5, 6, 13, 24");
+        Console.WriteLine("Select an Index of this Array.");
+        int index1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(intArray[index1]);
+
+        if (index1 > 4)
+        {
+            Console.WriteLine("That is out of the index range!");
+            Console.ReadLine();
+            return;
+        }
+        else
+        {
+            Console.WriteLine(intArray[index]);
+        }
+
+        List<string> stringList = new List<string>();
+        stringList.Add("Hello");
+        stringList.Add("Jello");
+        stringList.Add("Mellow");
+        stringList.Add("Yellow");
+        Console.WriteLine("Hello, Jello, Mellow, Yellow");
+        Console.WriteLine("Choose an Index for this Array.");
+        int index2 = Convert.ToInt32(Console.ReadLine());
+
+        if (index2 > 3)
+        {
+            Console.WriteLine("That is out of the index range!");
+            Console.ReadLine();
+            return;
+        }
+        else
+        {
+            Console.WriteLine(stringList[index]);
+            Console.Read();
+            return;
+        }
+           
     }
 }
+
